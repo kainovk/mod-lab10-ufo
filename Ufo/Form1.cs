c67b23ced992e0e52e37054459a909c327ce03b4
Kirill Kainov <kainov.dev@gmail.com>
2023-05-26 22:01:17 +0300

    add functions

---
+++ Ufo/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ufo
+{
+    public partial class Form1 : Form
+    {
+        private const int CellSize = 20; // Размер одной клетки в пикселях
+        private const int GridSize = 30; // Размер сетки (количество клеток в ширину и высоту)
+
+        private double startValue = 0.0; // Начальное значение x
+        private double endValue = 1.0; // Конечное значение x
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private float CalculateAngle(Point start, Point end)
+        {
+            float dx = end.X - start.X;
+            float dy = end.Y - start.Y;
+            return (float)Math.Atan2(dy, dx);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void InitializeGrid()
+        {
+            // Установка размера формы в зависимости от размера сетки
+            int formWidth = CellSize * GridSize + 1;
+            int formHeight = CellSize * GridSize + 1;
+            ClientSize = new Size(formWidth, formHeight);
+
+            // Создание и отрисовка сетки
+            var grid = new bool[GridSize, GridSize];
+            using (var graphics = CreateGraphics())
+            {
+                for (int x = 0; x < GridSize; x++)
+                {
+                    for (int y = 0; y < GridSize; y++)
+                    {
+                        int cellX = x * CellSize;
+                        int cellY = y * CellSize;
+                        var cellRect = new Rectangle(cellX, cellY, CellSize, CellSize);
+                        graphics.DrawRectangle(Pens.Black, cellRect);
+                        if (grid[x, y])
+                            graphics.FillRectangle(Brushes.Blue, cellRect);
+                    }
+                }
+            }
+        }
+
+        private void DrawTrajectory(Func<double, double> function)
+        {
+            // Очищение сетки и отрисовка траектории функции
+            var grid = new bool[GridSize, GridSize];
+            using (var graphics = CreateGraphics())
+            {
+                graphics.Clear(Color.White);
+                for (int x = 0; x < GridSize; x++)
+                {
+                    double value = startValue + (double)x / GridSize * (endValue - startValue);
+                    double functionValue = function(value);
+                    int y = (int)(functionValue * GridSize);
+                    if (x < GridSize && y < GridSize && x >= 0 && y >= 0)
+                    {
+                        grid[x, y] = true;
+                    }
+
+                    int cellX = x * CellSize;
+                    int cellY = y * CellSize;
+                    var cellRect = new Rectangle(cellX, cellY, CellSize, CellSize);
+                    graphics.DrawRectangle(Pens.Black, cellRect);
+                    if (x < GridSize && y < GridSize && x >= 0 && y >= 0 && grid[x, y])
+                        graphics.FillRectangle(Brushes.Blue, cellRect);
+                }
+            }
+        }
+
+        private void btnCos_Click(object sender, EventArgs e)
+        {
+            DrawTrajectory(Math.Cos);
+        }
+
+        private void btnXToThePowerOfX_Click(object sender, EventArgs e)
+        {
+            DrawTrajectory(x => Math.Pow(x, x));
+        }
+    }
+}
---
+++ Ufo/MyFunctions.cs
@@ -0,0 +1,164 @@
+﻿namespace Ufo
+{
+    class MyFunctions
+    {
+        public static double Exp(double x, int n)
+        {
+            double result = 1.0;
+            double term = 1.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= x / i;
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Sin(double x, int n)
+        {
+            double result = x;
+            double term = x;
+
+            for (int i = 1, j = 2; i < n; i++, j += 2)
+            {
+                term *= -x * x / (j * (j + 1));
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Cos(double x, int n)
+        {
+            double result = 1.0;
+            double term = 1.0;
+
+            for (int i = 1, j = 2; i < n; i++, j += 2)
+            {
+                term *= -x * x / (j * (j - 1));
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Sh(double x, int n)
+        {
+            double result = x;
+            double term = x;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= x * x / ((2 * i) * (2 * i + 1));
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Ch(double x, int n)
+        {
+            double result = 1.0;
+            double term = 1.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= x * x / ((2 * i) * (2 * i - 1));
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double PowerSeries(double x, double m, int n)
+        {
+            double result = 1.0;
+            double term = 1.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= m * x / i;
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double OnePlusX(double x, int n)
+        {
+            double result = 1.0;
+            double term = 1.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= x / i;
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double LnOnePlusX(double x, int n)
+        {
+            double result = 0.0;
+            double term = x;
+            int sgn = -1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                term *= sgn * x;
+                sgn = -sgn;
+
+                result += term / i;
+            }
+
+            return result;
+        }
+
+        public static double Arcsin(double x, int n)
+        {
+            double result = x;
+            double term = x;
+
+            for (int i = 1, j = 3; i < n; i++, j += 2)
+            {
+                term *= -(x * x * (j - 2)) / (j * (j - 1));
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Arctan(double x, int n)
+        {
+            double result = x;
+            double term = x;
+            int sgn = -1;
+
+            for (int i = 1, j = 3; i < n; i++, j += 2)
+            {
+                term *= sgn * (x * x * (j - 2)) / (j * (j - 1));
+                sgn = -sgn;
+                result += term;
+            }
+
+            return result;
+        }
+
+        public static double Tan(double x, int n)
+        {
+            double result = x;
+            double term = x;
+
+            for (int i = 1, j = 3; i < n; i++, j += 2)
+            {
+                term *= -x * x / (j * (j - 1));
+                result += term;
+            }
+
+            return result;
+        }
+    }
+}
