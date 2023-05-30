776aeefcc990885c2bde95072511866fb6706c7c
Kirill Kainov <kainov.dev@gmail.com>
2023-05-30 22:37:06 +0300

    graph

--- Ufo/Form1.Designer.cs
+++ Ufo/Form1.Designer.cs
@@ -29,24 +29,79 @@ namespace Ufo
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.chart1);
+            this.mainPanel.Controls.Add(this.startBtn);
+            this.mainPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(776, 426);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(632, 426);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(679, 360);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "button1";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.mainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button startBtn;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Ufo/Form1.cs
+++ Ufo/Form1.cs
@@ -1,111 +1,181 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing.Drawing2D;
 
 namespace Ufo
 {
     public partial class Form1 : Form
     {
-        private const int CellSize = 20; // Размер одной клетки в пикселях
-        private const int GridSize = 30; // Размер сетки (количество клеток в ширину и высоту)
-
-        private double startValue = 0.0; // Начальное значение x
-        private double endValue = 1.0; // Конечное значение x
+        private const string LEGEND_NAME = "Series";
+        private bool isGraphDrawn = false;
 
         public Form1()
         {
             InitializeComponent();
-            InitializeGrid();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private float CalculateAngle(Point start, Point end)
-        {
-            float dx = end.X - start.X;
-            float dy = end.Y - start.Y;
-            return (float)Math.Atan2(dy, dx);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-
-        }
-
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void InitializeGrid()
-        {
-            // Установка размера формы в зависимости от размера сетки
-            int formWidth = CellSize * GridSize + 1;
-            int formHeight = CellSize * GridSize + 1;
-            ClientSize = new Size(formWidth, formHeight);
-
-            // Создание и отрисовка сетки
-            var grid = new bool[GridSize, GridSize];
-            using (var graphics = CreateGraphics())
+            if (isGraphDrawn)
             {
-                for (int x = 0; x < GridSize; x++)
+                int stepSize = 2;
+                Point startPoint = new Point(50, 50);
+                Point endPoint = new Point(300, 300);
+                double tolerance = 0.5;
+                DrawGraph(startPoint, endPoint, stepSize, tolerance, e.Graphics);
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            InitializeChart();
+            int stepSize = 1;
+            Point startPoint = new Point(1, 10);
+            Point endPoint = new Point(510, 530);
+
+            for (double i = 1; i <= 101; i += 5)
+            {
+                int seriesMembers = CountSeriesMembers(startPoint, endPoint, stepSize, i);
+                AddDataToChart(i, seriesMembers);
+            }
+        }
+
+        private void InitializeChart()
+        {
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.ChartAreas.Add("mainArea");
+            chart1.Series.Add(LEGEND_NAME);
+            chart1.Series[LEGEND_NAME].ChartArea = "mainArea";
+            chart1.Series[LEGEND_NAME].ChartType = SeriesChartType.Spline;
+            chart1.Series[LEGEND_NAME].Color = Color.Brown;
+            chart1.ChartAreas[0].AxisX.Title = "Number of Series Members";
+            chart1.ChartAreas[0].AxisY.Title = "Accuracy";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+        }
+
+        private void DrawGraph(Point startPoint, Point endPoint, int stepSize, double tolerance, Graphics g)
+        {
+            DrawPoint(startPoint, g, tolerance);
+            DrawPoint(endPoint, g, tolerance);
+
+            double angle = CalculateAngle(startPoint, endPoint);
+            double distance = CalculateDistance(startPoint, endPoint);
+            double lastDistance = distance;
+            double x = startPoint.X;
+            double y = startPoint.Y;
+
+            while (distance <= lastDistance)
+            {
+                Thread.Sleep(50);
+                ClearPoint(x, y, g, tolerance);
+                lastDistance = CalculateDistance(new Point((int)x, (int)y), endPoint);
+                x += stepSize * MyFunctions.Cos(angle, 1);
+                y += stepSize * MyFunctions.Sin(angle, 1);
+                DrawPoint(new Point((int)x, (int)y), g, tolerance);
+                distance = CalculateDistance(new Point((int)x, (int)y), endPoint);
+                if (distance <= tolerance)
                 {
-                    for (int y = 0; y < GridSize; y++)
-                    {
-                        int cellX = x * CellSize;
-                        int cellY = y * CellSize;
-                        var cellRect = new Rectangle(cellX, cellY, CellSize, CellSize);
-                        graphics.DrawRectangle(Pens.Black, cellRect);
-                        if (grid[x, y])
-                            graphics.FillRectangle(Brushes.Blue, cellRect);
-                    }
+                    break;
                 }
             }
         }
 
-        private void DrawTrajectory(Func<double, double> function)
+        private void ClearPoint(double X, double Y, Graphics g, double radius)
         {
-            // Очищение сетки и отрисовка траектории функции
-            var grid = new bool[GridSize, GridSize];
-            using (var graphics = CreateGraphics())
+            using (Pen pen = new Pen(Color.White))
             {
-                graphics.Clear(Color.White);
-                for (int x = 0; x < GridSize; x++)
-                {
-                    double value = startValue + (double)x / GridSize * (endValue - startValue);
-                    double functionValue = function(value);
-                    int y = (int)(functionValue * GridSize);
-                    if (x < GridSize && y < GridSize && x >= 0 && y >= 0)
-                    {
-                        grid[x, y] = true;
-                    }
-
-                    int cellX = x * CellSize;
-                    int cellY = y * CellSize;
-                    var cellRect = new Rectangle(cellX, cellY, CellSize, CellSize);
-                    graphics.DrawRectangle(Pens.Black, cellRect);
-                    if (x < GridSize && y < GridSize && x >= 0 && y >= 0 && grid[x, y])
-                        graphics.FillRectangle(Brushes.Blue, cellRect);
-                }
+                g.DrawEllipse(pen, (float)X, (float)Y, (float)radius, (float)radius);
             }
         }
 
-        private void btnCos_Click(object sender, EventArgs e)
+        private void DrawPoint(Point point, Graphics g, double radius)
         {
-            DrawTrajectory(Math.Cos);
+            using (Pen pen = new Pen(Color.Black))
+            {
+                g.DrawEllipse(pen, point.X, point.Y, (float)radius, (float)radius);
+            }
         }
 
-        private void btnXToThePowerOfX_Click(object sender, EventArgs e)
+        private void AddDataToChart(double x, double y)
         {
-            DrawTrajectory(x => Math.Pow(x, x));
+            chart1.Series[LEGEND_NAME].Points.AddXY(x, y);
+        }
+
+        private int CountSeriesMembers(Point startPoint, Point endPoint, int stepSize, double tolerance)
+        {
+            int n = 1;
+            bool isFinished = false;
+            double angle;
+
+            while (n < 50 && !isFinished)
+            {
+                double ratio = CalculateRatio(startPoint, endPoint);
+                if (ratio <= 1)
+                {
+                    angle = MyFunctions.Arctan(ratio, n);
+                }
+                else
+                {
+                    ratio = 1 / ratio;
+                    angle = Math.PI / 2 - MyFunctions.Arctan(ratio, n);
+                }
+
+                double distance = CalculateDistance(startPoint, endPoint);
+                double lastDistance = distance;
+                double x = startPoint.X;
+                double y = startPoint.Y;
+
+                while (distance <= lastDistance)
+                {
+                    lastDistance = CalculateDistance(new Point((int)x, (int)y), endPoint);
+                    x += stepSize * MyFunctions.Cos(angle, n);
+                    y += stepSize * MyFunctions.Sin(angle, n);
+                    distance = CalculateDistance(new Point((int)x, (int)y), endPoint);
+                    if (distance <= tolerance)
+                    {
+                        isFinished = true;
+                        break;
+                    }
+                }
+
+                n++;
+            }
+
+            return n;
+        }
+
+        private double CalculateAngle(Point startPoint, Point endPoint)
+        {
+            int dx = Math.Abs(endPoint.X - startPoint.X);
+            int dy = Math.Abs(endPoint.Y - startPoint.Y);
+
+            double ratio = dy / (double)dx;
+            if (ratio <= 1)
+            {
+                return MyFunctions.Arctan(ratio, 1);
+            }
+            else
+            {
+                ratio = dx / (double)dy;
+                return Math.PI / 2 - MyFunctions.Arctan(ratio, 1);
+            }
+        }
+
+        private double CalculateDistance(Point point1, Point point2)
+        {
+            return Math.Sqrt(Math.Pow((point1.X - point2.X), 2) + Math.Pow((point1.Y - point2.Y), 2));
+        }
+
+        private double CalculateRatio(Point startPoint, Point endPoint)
+        {
+            int dx = Math.Abs(endPoint.X - startPoint.X);
+            int dy = Math.Abs(endPoint.Y - startPoint.Y);
+            return dy / (double)dx;
         }
     }
 }
